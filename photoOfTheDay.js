6d5edab051d8f1c755eb5d79964a0f9abb1b2e36
Samuel Gonzalez <samglz10@gmail.com>
2024-03-11 17:28:36 -0400

    POFTD and Nasa search libray

--- photoOfTheDay.js
+++ photoOfTheDay.js
@@ -1,34 +1,30 @@
 const fs = require('node:fs/promises');
 require('dotenv').config();
 const url = `https://api.nasa.gov/planetary/apod`;
-const searchURL = `https://images-api.nasa.gov/search`;
-const querySearch = `${process.argv[2]}`;
+const searchURL = `https://images-api.nasa.gov`;
+const date = `${process.argv[2]}`;
 const API_KEY = `${process.env.API_KEY}`;
 const params = new URLSearchParams({
     api_key: API_KEY,
-   // date,
-   q: querySearch,
+   //date,
 })
 
 
-// Photo Search
-
-
-async function NASAPhotoAndImageLibrary(url, date){
-
-    if(date === undefined || date === NaN){
+//Photo of the day
+ async function getPhotos(url, date){
+    if(date === undefined){
         console.log('check')
         return new Error('Please enter a valid date in the following format')
     }
     try {
     const response = await fetch(`${url}?${params}`)
-    console.log(response.url)
+    //console.log(response.url)
     const result = await response.json();
     console.log(result)
     } 
     catch {
         throw new Error('fetch failed')
     }
-    
 }
-NASAPhotoAndImageLibrary(searchURL);
+
+getPhotos(url, date)
