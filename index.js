aff44f89113425e89d63755cb68aacbd131cf603
Samuel Gonzalez <samglz10@gmail.com>
2024-03-11 18:48:30 -0400

    getting limited return search

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ require('dotenv').config();
 const url = `https://api.nasa.gov/planetary/apod`;
 const searchURL = `https://images-api.nasa.gov/search`;
 const querySearch = `${process.argv[2]}`;
+const pageSize = Number(process.argv[3]);
 const API_KEY = `${process.env.API_KEY}`;
 const params = new URLSearchParams({
-    api_key: API_KEY,
+    //api_key: API_KEY,
    // date,
    q: querySearch,
+   page_size: pageSize,
 })
 
 
 // Photo Search
 
 
-async function NASAPhotoAndImageLibrary(url, date){
-
-    if(date === undefined || date === NaN){
-        console.log('check')
-        return new Error('Please enter a valid date in the following format')
-    }
-    try {
-    const response = await fetch(`${url}?${params}`)
-    console.log(response.url)
-    const result = await response.json();
-    console.log(result)
+async function NASAPhotoAndImageLibrary(url){
+   if(params === undefined || !isNaN(params.page_size) ){
+        console.log('error')
+        return console.log(new Error('Please specify a search term')) 
     } 
-    catch {
-        throw new Error('fetch failed')
+    else {
+        try {
+        const response = await fetch(`${url}?${params}`)
+        console.log(response.url)
+        const result = await response.json();
+        const searchResulst = result.collection.items;
+        console.log(searchResulst)
+        const 
+        } 
+        catch {
+            throw new Error('Once you entered the search enter a limit');
+        }
     }
-    
 }
 NASAPhotoAndImageLibrary(searchURL);
