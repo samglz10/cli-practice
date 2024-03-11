5858f29165ea2d665fba7c86a44e4a1ec7114ff3
Samuel Gonzalez <samglz10@gmail.com>
2024-03-11 13:07:07 -0400

    wokring request

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const fs = require('node:fs/promises');
+require('dotenv').config();
 const url = `https://api.nasa.gov/planetary/apod`;
 const date = `${process.argv[2]}`;
-const API_KEY = 
-const total_results = 2;
+const API_KEY = `${process.env.API_KEY}`;
+console.log(API_KEY);
+
 
  async function getPhotos(url){
     const params = new URLSearchParams({
+        api_key: API_KEY,
         date,
-        
     })
     const response = await fetch(`${url}?${params}`)
+    console.log(response.url)
     const result = await response.json();
+    console.log(result)
 }
 
 getPhotos(url)
