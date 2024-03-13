28248917dbeb2a4db2f490e16e4e4d7b113c8565
Samuel Gonzalez <samglz10@gmail.com>
2024-03-12 20:36:08 -0400

    getting image title and url download

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
 const fs = require('node:fs/promises');
 require('dotenv').config();
-const url = `https://api.nasa.gov/planetary/apod`;
-const searchURL = `https://images-api.nasa.gov/search`;
+
+// const url = 'https://api.nasa.gov/planetary/apod';
+const searchURL = 'https://images-api.nasa.gov/search';
 const querySearch = `${process.argv[2]}`;
 const pageSize = Number(process.argv[3]);
-const API_KEY = `${process.env.API_KEY}`;
+// const API_KEY = `${process.env.API_KEY}`;
 const params = new URLSearchParams({
-    //api_key: API_KEY,
-   // date,
-   q: querySearch,
-   page_size: pageSize,
-   media_type:'image',
-})
-
+  // api_key: API_KEY,
+  // date,
+  q: querySearch,
+  page_size: pageSize,
+  media_type: 'image',
+});
 
 // Photo Search
-async function NASAPhotoAndImageLibrary(url){
-   if(params === undefined || !isNaN(params.page_size) ){
-        console.log('error')
-        return console.log(new Error('Please specify a search term')) 
-    } 
-    else {
-        try {
-        const response = await fetch(`${url}?${params}`)
-        console.log(response.url)
-        const result = await response.json();
-        const searchResults = result.collection.items;
-        console.log(searchResults)
-        console.log(searchResults);
-        for(const item of searchResults){
-            console.log(item);
-            const image = item.links[0].href
-            console.log(image);
-        }
-
-
-        } 
-        catch {
-            throw new Error('Whoops something broke');
-        }
+async function NASAPhotoAndImageLibrary(url) {
+  if (params === undefined || !isNaN(params.page_size)) {
+    console.log('error');
+    return console.log(new Error('Please specify a search term'));
+  }
+  try {
+    const response = await fetch(`${url}?${params}`);
+    console.log(response.url);
+    const result = await response.json();
+    const searchResults = result.collection.items;
+    //console.log(searchResults);
+    for (const item of searchResults) {
+      console.log(item);
+      const  title  = item.data[0];
+      const image = item.links[0].href;
+      console.log(image);
+      //const path = path.join(__dirname, `${title}.jpg`);
     }
+  } catch {
+    throw new Error('Whoops something broke');
+  }
 }
 NASAPhotoAndImageLibrary(searchURL);
-
--- photoOfTheDay.js
+++ photoOfTheDay.js
@@ -1,30 +1,29 @@
 const fs = require('node:fs/promises');
 require('dotenv').config();
-const url = `https://api.nasa.gov/planetary/apod`;
-const searchURL = `https://images-api.nasa.gov`;
+
+const url = 'https://api.nasa.gov/planetary/apod';
+// const searchURL = 'https://images-api.nasa.gov'
 const date = `${process.argv[2]}`;
 const API_KEY = `${process.env.API_KEY}`;
 const params = new URLSearchParams({
-    api_key: API_KEY,
-   //date,
-})
+  api_key: API_KEY,
+  // date,
+});
 
-
-//Photo of the day
- async function getPhotos(url, date){
-    if(date === undefined){
-        console.log('check')
-        return new Error('Please enter a valid date in the following format')
-    }
-    try {
-    const response = await fetch(`${url}?${params}`)
-    //console.log(response.url)
+// Photo of the day
+async function getPhotos(url, date) {
+  if (date === undefined) {
+    console.log('check');
+    return new Error('Please enter a valid date in the following format');
+  }
+  try {
+    const response = await fetch(`${url}?${params}`);
+    // console.log(response.url)
     const result = await response.json();
-    console.log(result)
-    } 
-    catch {
-        throw new Error('fetch failed')
-    }
+    console.log(result);
+  } catch {
+    throw new Error('fetch failed');
+  }
 }
 
-getPhotos(url, date)
+getPhotos(url, date);
